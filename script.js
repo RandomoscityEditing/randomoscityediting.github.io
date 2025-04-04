3f73192c4720f2ad8f39dfc6c17455caa223efd9
masonshields92 <masonshields92@yahoo.com>
2025-04-04 15:43:13 -0500

    Add files via upload
    Adding files for website

---
+++ script.js
@@ -0,0 +1,25 @@
+function contactMe(){
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
+
+const serviceID = "service_myhqf7f";
+const templateID = "template_gw5yz1o";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your email has been sent. Thank you!");
+
+    })
+    .catch(err=>console.log(err));
+}
